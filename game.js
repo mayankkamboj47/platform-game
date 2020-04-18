b5456e9dfbe6434d6b3dd51027c77ca0fdbdce29
Mayank Kamboj <mayankkamboj47@gmail.com>
2020-04-18 11:54:50 +0530

    Added the display

--- game.js
+++ game.js
@@ -185,6 +185,100 @@ class Level{
     * doing anything yet, apart from coming into existence and holding a bunch
     * of properties
     */
-   const simpleLevel = new Level(simpleLevelPlan);
-   console.log(`${simpleLevel.width} by ${simpleLevel.height}`);
-   console.log("ehl")
+
+   /**Now, we start writing the drawing program */
+   function elt(name,attrs,...children){
+       let element = document.createElement(name);
+       console.log('attrs',attrs)
+       for(let attr in attrs){
+           element.setAttribute(attr,attrs[attr]);
+       }
+       for(let child of children){
+           element.appendChild(child);
+       }
+       return element;
+   }
+   /**Now we write the class for our display */
+   class DOMDisplay{
+       constructor(parent,level){
+           this.dom = elt('div',{class:'game'},drawGrid(level));
+           this.actorLayer = null;
+           parent.appendChild(this.dom);
+       }
+       clear(){
+           this.dom.remove();
+       }
+   }
+   /**Now, this is interesting. What we want to see is
+    * how all these different classes that all hold actors in one form
+    * or another fit into each other
+    * Also, we have another function drawGrid
+    */
+   const scale = 20;
+   function drawGrid(level){
+       return elt('table',{class:'background',
+    style:`width:${level.width*scale}px`},
+    ...level.rows.map(row=>{
+        console.log(row);
+        return elt('tr',{style:`height:${scale}px`},...row.map(type=>elt('td',{class:type})));
+    }))
+   }
+   /**We draw actors seperately in a different div */
+   function drawActors(actors){
+       return elt('div',{},...actors.map((actor)=>{
+           let rect = elt('div',{class:`actor ${actor.type}`});
+           rect.style.height = `${actor.size.y*scale}px`
+           rect.style.width = `${actor.size.x*scale}px`
+           rect.style.left = `${actor.pos.x*scale}px`
+           rect.style.top = `${actor.pos.y*scale}px`
+           return rect;
+       }));
+   }
+   /**Now, a way for DOMDisplay to refresh */
+   DOMDisplay.prototype.syncState = function(state){
+       if(this.actorLayer) this.actorLayer.remove();
+       this.actorLayer = drawActors(state.actors);
+       this.dom.appendChild(this.actorLayer);
+       this.dom.className = `game ${state.status}`;
+       this.scrollPlayerIntoView(state);
+   }
+   /**Now, this raises a few questions for me. Why are we setting this.dom.className again here
+    * 
+    * Is it changing in the middle ? We're redrawing the actors that's all, can game state change in that process?
+    * Also, we see another person in the plot, scrollPlayerIntoView, which is likely a camera that follows the
+    * player. It will be fun to implement
+    */
+   DOMDisplay.prototype.scrollPlayerIntoView = function(state){
+       let width = this.dom.clientWidth;
+       let height = this.dom.clientHeight;
+       let left = this.dom.scrollLeft;
+       let top = this.dom.scrollTop;
+       let right = left + width;
+       let bottom = top + height;
+       let margin = width/3;
+       let player = state.player;
+       let center = player.pos.plus(player.size.times(0.5)).times(scale);
+       if(center.x<left+margin){
+           this.dom.scrollLeft = center.x - margin;
+       }
+       else if(center.x<right-margin){
+           this.dom.scrollLeft = center.x + margin - width;
+       }
+       if(center.y<top+margin){
+           this.dom.scrollTop = center.y - margin;
+       }
+       else if(center.y>bottom-margin){
+           this.dom.scrollTop = center.y + margin - height;
+       }
+   }
+   /**The above function is interesting in that there is some layout math going on
+    * To figure that math, I will probably write another document someday. I do have a rough draft of why
+    * this works the way it does. Which I'll put online anyway
+    */
+   window.onload = function(){
+       let simpleLevel = new Level(simpleLevelPlan);
+       debugger;
+       let display = new DOMDisplay(document.body,simpleLevel);
+       debugger;
+       display.syncState(State.start(simpleLevel));
+   }
